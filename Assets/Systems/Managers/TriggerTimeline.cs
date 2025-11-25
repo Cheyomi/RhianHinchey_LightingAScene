505557395aeed3b2497034df3b339f48972ee761
Rhian <139021661+Cheyomi@users.noreply.github.com>
2025-11-25 12:26:57 -0400

    Added Guapo Timeline
    - First part

---
+++ Assets/Systems/Managers/TriggerTimeline.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TriggerScript : MonoBehaviour
+{
+
+    [SerializeField] private PlayableDirector playableDirector;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        playableDirector.Play();
+    }
+}
