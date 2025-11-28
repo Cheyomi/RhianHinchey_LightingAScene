cbacea745cc141a811296663dc22ae92f2387b58
Rhian <139021661+Cheyomi@users.noreply.github.com>
2025-11-28 02:55:54 -0400

    Added all triggers and started main menu

--- Assets/Systems/Managers/TriggerTimeline.cs
+++ Assets/Systems/Managers/TriggerTimeline.cs
@@ -2,13 +2,21 @@ using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
 
+
 public class TriggerScript : MonoBehaviour
 {
+
+    public bool isPlayed;
 
     [SerializeField] private PlayableDirector playableDirector;
 
     public void OnTriggerEnter(Collider other)
     {
         playableDirector.Play();
+
+        GetComponent<MeshRenderer>().enabled = false;
+
     }
+
+
 }
